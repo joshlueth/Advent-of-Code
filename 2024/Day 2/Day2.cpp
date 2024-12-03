03d1630a888e34f337905714e13a416f09a097b0
Joshua Lueth <lueth020@umn.edu>
2024-12-03 03:26:13 -0600

    Solved Day 2, part 1

--- 2024/Day 2/Day2.cpp
+++ 2024/Day 2/Day2.cpp
@@ -1,23 +1,11 @@
-// PROBLEM
-// given two lists of integers, of the same length, in file input.txt:
-// sort each of the lists in ascending order
-// find the sum of the distance (absolute value of the differences) between each entry in the sorted lists
-
-// SOLUTION
-// Read in input file
-// Into list1, list2
-// Call sorting algorithm on list1, list2
-// loop through the length, adding the distance to a sum variable
-// print sum variable
-
 #include "Day2.h"
 #include <fstream>
 #include <sstream>
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
 #include <cmath>
+#include <bits/stdc++.h>
 
 int main()
 {
@@ -31,93 +19,48 @@ int main()
 		return 1;
 	}
 
-	std::vector<int> list1 {};
-	std::vector<int> list2 {};
-
-
 	std::string inputStr {};
+	int safe{0}, safeDamp{0};
 	while (std::getline(inputFile,inputStr))
 	{
-		int val1{}, val2{};
 		std::stringstream ss (inputStr);
-		ss >> val1 >> val2;
-		list1.push_back(val1);
-		list2.push_back(val2);
+		std::vector<int> list(std::istream_iterator<int>(ss),{});
+		int vecDiffSign{}, increment{1}, incrementDamp{1};
+		for (std::size_t i{0}; i<list.size()-1; ++i)
+		{
+			int diff {list[i+1]-list[i]};
+			int diffSign {sign(diff)};
+			if (i==0) 
+			{
+				vecDiffSign = diffSign;
+				if (vecDiffSign==0) 
+				{
+					increment = 0;
+					break;
+				}
+			}
+			if (diffSign!=vecDiffSign || abs(diff)>3) 
+			{
+				increment = 0;
+				break;
+			}
+		}
+		safe += increment;
+		safeDamp += incrementDamp;
 	}
 
 	inputFile.close();
 
-	std::sort(list1.begin(),list1.end());
-	std::sort(list2.begin(),list2.end());
+	std::cout << "Number of Safe Reports: " << safe << "\n";
+	std::cout << "Number of Safe Reports After Damping: " << safeDamp << "\n";
 
-	std::size_t len{};
+	return 0;
+}
 
-	len = list1.size();
-	if (len != list2.size())
-	{
-		std::cerr << "Lists are not of equal length\n";
-		return 1;
-	}
-
-	int sum {0};
-
-	std::size_t ii{0};
-	for (; ii<len; ++ii)
-	{
-		sum += abs(list1[ii]-list2[ii]);
-	}
-
-	std::cout << "Sum of distances: " << sum <<"\n";
-
-
-// second problem:
-// go through each entry in the first list and calculate how many times it appears in the second list
-// the similarity score is the number multiplied by the amount of times it appears
-// in order to avoid using brute force, we can use the sorted lists and 'pointer traversal' of some sort
-// For each entry in the list:
-//   check if the entry is equal to the prior one, in which case we simply add the similarity score again
-//   if not, check the entry of the second list corresponding to the pointer
-//   if it is the same, add one to counter and increment pointer, continue loop
-//   if it is not the same, stop
-	std::size_t pointer{0};
-	int counter;
-	int simScore{0};
-
-	ii = 0;
-	for (; ii<len; ++ii)
-	{
-		// check if the entry is the same as the preceding one
-		// std::cout << "list 1: " << list1[ii] << "\n";
-		if (ii!=0)
-		{
-			if (list1[ii]==list1[ii-1])
-			{
-				simScore += list1[ii]*counter;
-				continue;
-			}
-		}
-		// if not, use the pointer variable to calculate the similarity score
-		while (list2[pointer]<list1[ii]) 
-		{
-			// std::cout << "list 2: " << list2[pointer] << "\n";
-			pointer++;
-		}
-
-		counter = 0;
-		while (list2[pointer]==list1[ii])
-		{
-			// std::cout << "list 2: " << list2[pointer] << "\n";
-			counter++;
-			pointer++;
-		}
-		// if (counter!=0) std::cout << "simScore: " << list1[ii] << " " << counter << "\n";
-		simScore += list1[ii]*counter;
-		// if (counter!=0) std::cout << "Updates: " << list1[ii]*counter << " " << simScore << "\n";
-
-	}
-
-	std::cout << "Similarity score: " << simScore << "\n";
-
+int sign(int number)
+{
+	if (number>0) return 1;
+	if (number<0) return -1;
 	return 0;
 }
 
