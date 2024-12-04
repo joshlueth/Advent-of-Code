7788205f867347886454d0fee52f6d90b9c74483
Joshua Lueth <lueth020@umn.edu>
2024-12-03 21:03:35 -0600

    Solved Day 3, part 1

--- 2024/Day 3/Day3.cpp
+++ 2024/Day 3/Day3.cpp
@@ -1,11 +1,7 @@
-#include "Day2.h"
+#include "Day3.h"
 #include <fstream>
-#include <sstream>
 #include <iostream>
-#include <vector>
 #include <string>
-#include <cmath>
-#include <bits/stdc++.h>
 #include <regex>
 
 int main()
@@ -21,19 +17,30 @@ int main()
 	}
 
 	std::string inputStr {};
-	int mult{1};
+	int sum{0};
 	while (std::getline(inputFile,inputStr))
 	{
+		std::regex multFormat {"mul\\([0-9]+,[0-9]+\\)"};
+		for (std::smatch strMatch{}; std::regex_search(inputStr, strMatch, multFormat);)
+		{
+			std::regex multNumber {"[0-9]+"};
+			std::string multStr {strMatch.str()};
+			int mult{1};
+			for (std::smatch numMatch{}; std::regex_search(multStr, numMatch, multNumber);)
+			{
+				mult *= std::stoi(numMatch.str());
+			  multStr = numMatch.suffix();
+			}
+			sum += mult;
+			inputStr = strMatch.suffix();
 
-
-		
+		}
 
 	}
 
 	inputFile.close();
 
-
-	std::cout << "Total Multiplcation: " << mult << "\n";
+	std::cout << "Total Multiplcation: " << sum << "\n";
 
 	return 0;
 }
