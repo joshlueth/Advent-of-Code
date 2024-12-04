bd5b349c67edb470ff38ff4ff80b9b39e49483a1
Joshua Lueth <lueth020@umn.edu>
2024-12-04 02:11:16 -0600

    Solved Day 3, part 3

--- 2024/Day 3/Day3.cpp
+++ 2024/Day 3/Day3.cpp
@@ -34,20 +34,32 @@ int main()
 	bool multEnabled {true};
 	while (std::getline(inputFile,inputStr))
 	{
-		std::regex enabled {".+?(don't\\(\\)|$)"};
-		for (std::smatch enabledMatch{}; std::regex_search(inputStr, enabledMatch, enabled);)
+		std::string currentStr {};
+		currentStr = inputStr;
+		while (currentStr!="")
 		{
-			std::string enabledStr {enabledMatch.str()};
-			multEnabled = false;
-			sum += doMultOnStr(enabledStr);
+			if (multEnabled)
+			{
+				std::regex enabled {"(don't\\(\\))|$"};
+				std::smatch enabledMatch{}; 
+				std::regex_search(currentStr, enabledMatch, enabled);
+				sum += doMultOnStr(enabledMatch.prefix());
+				currentStr = enabledMatch.suffix();
+				if (enabledMatch[0]=="") break;
+				multEnabled = false;
+				if (currentStr=="") break;
+			}
 
-			std::regex disabled {"do\\(\\)"};
-			std::smatch disabledMatch{};
-			std::string disabledStr {enabledMatch.suffix()};
-			std::regex_search(disabledStr,disabledMatch,disabled);
-
-			inputStr = disabledMatch.suffix();
-
+			if (!multEnabled)
+			{
+				std::regex disabled {"do\\(\\)"};
+				std::smatch disabledMatch{};
+				std::regex_search(currentStr,disabledMatch,disabled);
+				currentStr = disabledMatch.suffix();
+				if (disabledMatch[0]=="") break;
+				multEnabled = true;
+				if (currentStr=="") break;
+			}
 		}
 
 	}
@@ -61,6 +73,7 @@ int main()
 
 int doMultOnStr(std::string string)
 {
+
 	int sum {0};
 	std::regex multFormat {"mul\\([0-9]+,[0-9]+\\)"};
 	for (std::smatch strMatch{}; std::regex_search(string, strMatch, multFormat);)
@@ -71,15 +84,12 @@ int doMultOnStr(std::string string)
 		int mult {1};
 		while (std::regex_search(multStr, numMatch, multNumber))
 		{
-
 			mult *= std::stoi(numMatch.str());
 			multStr = numMatch.suffix();
 
 		}
 		sum += mult;
 		string = strMatch.suffix();
-
 	}
-
 	return sum;
 }
