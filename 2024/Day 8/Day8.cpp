36e7a2a2362b5646441230ee78311b22cb46284a
Joshua Lueth <lueth020@umn.edu>
2024-12-08 01:40:15 -0600

    Solved Day 8, parts 1 and 2

--- 2024/Day 8/Day8.cpp
+++ 2024/Day 8/Day8.cpp
@@ -1,11 +1,18 @@
-#include "Day7.h"
+#include "Day8.h"
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
+#include <unordered_map>
+#include <set>
+#include <utility>
+#include <chrono>
 
 int main()
 {
-	std::string fileName {"sample.txt"};
+	auto t1 {std::chrono::high_resolution_clock::now()};
+
+	std::string fileName {"input.txt"};
 
 	std::ifstream inputFile {fileName};
 	if (!inputFile)
@@ -15,15 +22,97 @@ int main()
 	}
 
 	std::string inputStr {};
+	std::vector<std::vector<char>> map {};
 	while (std::getline(inputFile,inputStr))
 	{
-		
+		std::vector<char> intoArray(inputStr.begin(),inputStr.end());
+		map.push_back(intoArray);
 	}
 
 	inputFile.close();
 
+	auto t2 {std::chrono::high_resolution_clock::now()};
+
+	std::set<char> keys {};
+	std::unordered_multimap<char,std::pair<int,int>> nodes {};
+	for (std::size_t rw {0}; rw<map.size(); rw++) {
+		for (std::size_t cl{0}; cl<map[0].size(); cl++) {
+			if (map[rw][cl]!='.') {
+				keys.insert(map[rw][cl]);
+				nodes.insert({map[rw][cl],std::make_pair(int(rw),int(cl))});
+			}
+		}
+	}
+
+	auto t3 {std::chrono::high_resolution_clock::now()};
+
+	int szx{int(map.size())}, szy{int(map[0].size())};
+	std::set<std::pair<int,int>> antinodes1{}, antinodes2{};
+	for (const auto& k : keys) {
+		auto range {(nodes.equal_range(k))};
+		for (auto loc1{range.first}; loc1!=range.second; ++loc1) {
+			for (auto loc2 {std::next(loc1)}; loc2!=range.second; ++loc2) {
+				int x1{}, y1{}, x2{}, y2{}, dx{}, dy{};
+				getInfo(x1, y1, x2, y2, dx, dy, loc1->second, loc2->second);
+				an1(x1, y1, x2, y2, dx, dy, szx, szy, antinodes1);
+				an2(x1, y1, x2, y2, dx, dy, szx, szy, antinodes2);
+			}
+		}
+	}
+
+  std::size_t antinodeNumber1 {std::size(antinodes1)};
+	std::size_t antinodeNumber2 {std::size(antinodes2)};
+
+	auto t4 {std::chrono::high_resolution_clock::now()};
+
+ 	std::cout << "Number of unique antinodes for part 1 is " << antinodeNumber1 << "\n";
+ 	std::cout << "Number of unique antinodes for part 2 is " << antinodeNumber2 << "\n";
+
+	std::cout << "Program took, in microseconds:" << "\n";
+	std::cout << "  Reading file         : " << std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count() << "\n";
+	std::cout << "  Getting nodes, keys  : " << std::chrono::duration_cast<std::chrono::microseconds>(t3-t2).count() << "\n";
+	std::cout << "  Antinode calculations: " << std::chrono::duration_cast<std::chrono::microseconds>(t4-t3).count() << "\n";
+	std::cout << "For a total of " << std::chrono::duration_cast<std::chrono::microseconds>(t4-t1).count() << "\n";
 
 
 	return 0;
 }
 
+bool inBounds(int val, int sz) {
+	if (val>=0 && val <sz) return true;
+	return false;
+}
+
+void an1(int x1, int y1, int x2, int y2, int dx, int dy, int szx, int szy, std::set<std::pair<int,int>>& ans) {
+	if (inBounds(x1-dx,szx) && inBounds(y1-dy,szy)) ans.insert(std::make_pair(x1-dx,y1-dy));
+	if (inBounds(x2+dx,szx) && inBounds(y2+dy,szy)) ans.insert(std::make_pair(x2+dx,y2+dy));
+}
+
+void an2(int x1, int y1, int x2, int y2, int dx, int dy, int szx, int szy, std::set<std::pair<int,int>>& ans) {
+	int anx {}, any{};
+	anx = x2;
+	any = y2;
+	// forward stepping
+	while (inBounds(anx, szx) && inBounds(any,szy)) {
+		ans.insert(std::make_pair(anx, any));
+		anx += dx;
+		any += dy;
+	}
+	anx = x1;
+	any = y1;
+	// backward stepping
+	while (inBounds(anx, szx) && inBounds(any,szy)) {
+		ans.insert(std::make_pair(anx, any));
+		anx -= dx;
+		any -= dy;
+	}
+} 
+
+void getInfo(int& x1, int& y1, int& x2, int& y2, int& dx, int& dy, std::pair<int,int> loc1, std::pair<int,int> loc2) {
+	x1 = loc1.first;
+	y1 = loc1.second;
+	x2 = loc2.first;
+	y2 = loc2.second;
+	dx = x2 - x1;
+	dy = y2 - y1;
+}
--- 2024/Day 8/Day8.h
+++ 2024/Day 8/Day8.h
@@ -1,2 +1,8 @@
 #pragma once
+#include <set>
+#include <utility>
 
+bool inBounds(int,int);
+void an1(int, int, int, int, int, int, int, int, std::set<std::pair<int,int>>&);
+void an2(int, int, int, int, int, int, int, int, std::set<std::pair<int,int>>&);
+void getInfo(int&, int&, int&, int&, int&, int&, std::pair<int,int>, std::pair<int,int>);
