5126d833169c9a4daafb836cf7ff5870a1773713
Joshua Lueth <lueth020@umn.edu>
2024-12-10 16:13:42 -0600

    Day 11 Scaffolding

---
+++ 2024/Day 11/Day11.cpp
@@ -0,0 +1,36 @@
+#include "Day11.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <chrono>
+
+int main()
+{
+	auto t1 {std::chrono::high_resolution_clock::now()};
+
+	std::string fileName {"sample.txt"};
+
+	std::ifstream inputFile {fileName};
+	if (!inputFile)
+	{
+		std::cerr << "Input file " << fileName << " could not be opened\n";
+		return 1;
+	}
+
+	std::string inputStr {};
+
+	while (std::getline(inputFile,inputStr))
+	{
+
+	}
+
+	inputFile.close();
+
+
+	auto t2 {std::chrono::high_resolution_clock::now()};
+
+	std::cout << "Program took, in microseconds:" << "\n";
+	std::cout << "  Total Time: " << std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count() << "\n";
+
+	return 0;
+}
