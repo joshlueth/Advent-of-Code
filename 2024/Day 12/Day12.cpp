aaea5b66caae1d9250283928e514b8e5b65c5a21
Joshua Lueth <lueth020@umn.edu>
2024-12-12 00:57:27 -0600

    Solved Day 12, part 1

--- 2024/Day 12/Day12.cpp
+++ 2024/Day 12/Day12.cpp
@@ -3,12 +3,15 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <vector>
+#include <set>
+#include <utility>
 
 int main()
 {
 	auto t1 {std::chrono::high_resolution_clock::now()};
 
-	std::string fileName {"sample.txt"};
+	std::string fileName {"input.txt"};
 
 	std::ifstream inputFile {fileName};
 	if (!inputFile)
@@ -18,19 +21,115 @@ int main()
 	}
 
 	std::string inputStr {};
-
+  std::vector<std::vector<char>> map{};
 	while (std::getline(inputFile,inputStr))
 	{
-
+    std::vector<char> intoArray(inputStr.begin(),inputStr.end());
+    map.push_back(intoArray);
 	}
 
 	inputFile.close();
 
-
 	auto t2 {std::chrono::high_resolution_clock::now()};
 
+// okay so the plan of attack is simple
+// create a boolean array of equal size checking if we have already gone through that cell
+// then go through each element in map
+// if already traversed, keep going
+// if not, create regionA with 1 and regionP with 4 and enter the recursive traversal of all neighboring cells
+// when recursively going into a neighboring cell
+  // not of a neighboring type, return
+  // of a neighboring type and traversed, decrease perimiter count by 1
+  // of a neighboring type and not traversed, add to traverse queue (which is a set)
+
+  int ap {0};
+  std::vector<std::vector<bool>> traversed(map.size(),std::vector<bool>(map[0].size(),false));
+  for (std::size_t rw {0}; rw<map.size(); ++rw) {
+    for (std::size_t ch {0}; ch<map[0].size(); ++ch) {
+      if (traversed[rw][ch]) continue;
+// std::cout << rw << " " << ch << " " << map[rw][ch] << "\n";
+      ap += region(map,traversed,rw,ch);
+    } 
+  }
+
+  auto t3 {std::chrono::high_resolution_clock::now()};
+
+
+	auto t4 {std::chrono::high_resolution_clock::now()};
+
+  std::cout << "Sum of Area multiplied by Perimeter for all regions: " << ap << "\n";
 	std::cout << "Program took, in microseconds:" << "\n";
-	std::cout << "  Total Time: " << std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count() << "\n";
+	std::cout << "  Total Time: " << std::chrono::duration_cast<std::chrono::microseconds>(t4-t1).count() << "\n";
+	std::cout << "  Reading in File: " << std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count() << "\n";
+	std::cout << "  Problem Solving: " << std::chrono::duration_cast<std::chrono::microseconds>(t3-t1).count() << "\n";
+
 
 	return 0;
 }
+
+int region(const std::vector<std::vector<char>>& map, std::vector<std::vector<bool>>& traversed, std::size_t rw, std::size_t ch ) {
+  std::pair<int,int> ap {std::make_pair(0,0)};
+  std::set<std::pair<std::size_t,std::size_t>> queue={std::make_pair(rw,ch)};
+  while (!queue.empty()){
+    cell(map,traversed,queue,ap);
+  }
+// std::cout << "ap: " << ap.first << " " << ap.second << "\n";
+// std::cout << "is queue empty? " << queue.empty() << " " << queue.size() << "\n";
+// for (auto pr : queue) {
+//   std::cout << "queue entries: " << pr.first << " " << pr.second << "\n";
+// }
+  return ap.first*ap.second;
+}
+
+void cell(const std::vector<std::vector<char>>& map, std::vector<std::vector<bool>>& traversed,
+          std::set<std::pair<std::size_t,std::size_t>>& queue, std::pair<int,int>& ap) {
+// by definition, have not traversed a cell in the queue, and it is of the same region
+  auto cl = *queue.begin();
+// for (auto i:traversed) {
+//   for (auto j:i) {
+//     std::cout << j;
+//   }
+//   std::cout << "\n";
+// }
+// std::cout << "location chosen: " << cl.first << " " << cl.second << " map size " << map.size() << " " << map[0].size() << "\n";
+  ap.first+=1;
+  ap.second+=4;
+  if (cl.first>0) {
+    if (map[cl.first-1][cl.second]==map[cl.first][cl.second]) {
+      ap.second-=1;
+      if (!traversed[cl.first-1][cl.second]) {
+        queue.insert(std::make_pair(cl.first-1,cl.second));
+      }
+    }
+  }             
+  if (cl.first+1<map.size()) {
+    if (map[cl.first+1][cl.second]==map[cl.first][cl.second]) {
+      ap.second-=1;
+      if (!traversed[cl.first+1][cl.second]) {
+        queue.insert(std::make_pair(cl.first+1,cl.second));
+      }
+    }
+  }
+  if (cl.second>0) {
+    if (map[cl.first][cl.second-1]==map[cl.first][cl.second]) {
+      ap.second-=1;
+      if (!traversed[cl.first][cl.second-1]) {
+        queue.insert(std::make_pair(cl.first,cl.second-1));
+      }
+    }
+  }
+  if (cl.second+1<map[0].size()){
+    if (map[cl.first][cl.second+1]==map[cl.first][cl.second]) {
+      ap.second-=1;
+      if (!traversed[cl.first][cl.second+1]) {
+        queue.insert(std::make_pair(cl.first,cl.second+1));
+      }
+    }
+  }
+  traversed[cl.first][cl.second] = true;
+// for (auto pr : queue) {
+//   std::cout << "queue entries in cell: " << pr.first << " " << pr.second << "\n";
+// }
+  queue.erase(cl);
+}
+
--- 2024/Day 12/Day12.h
+++ 2024/Day 12/Day12.h
@@ -1,2 +1,8 @@
 #pragma once
+#include <vector>
+#include <set>
+#include <utility>
 
+int region(const std::vector<std::vector<char>>&, std::vector<std::vector<bool>>&, std::size_t, std::size_t);
+void cell(const std::vector<std::vector<char>>&, std::vector<std::vector<bool>>&,
+          std::set<std::pair<std::size_t,std::size_t>>&, std::pair<int,int>&);
