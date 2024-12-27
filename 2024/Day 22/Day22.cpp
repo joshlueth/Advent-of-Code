6abf01399b9b84fe6c1771aa5a22d57b8c61fd91
Joshua Lueth <lueth020@umn.edu>
2024-12-26 21:15:49 -0600

    Day 22 Scaffolding

---
+++ 2024/Day 22/Day22.cpp
@@ -0,0 +1,63 @@
+#include "Day22.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <chrono>
+
+int main(int argc, char* argv[])
+{
+
+// argc is the number of command line arguments
+// argv is the value of command line arguments
+// currently accept argc==1 (no additional) or argc==2 (1 additional, filename w/ no .txt)
+
+  std::string fileName {"sample.txt"}, extensionDefault {".txt"};
+  if (argc<=1) {
+  } else if (argc==2) {
+    fileName = argv[1];
+    if (fileName.find('.')==std::string::npos) {
+      fileName += extensionDefault;
+    } 
+  } else {
+    std::cerr << "Received " << argc-1 << " additional command line arguments" << "\n";
+    std::cerr << "Only accepts up to 1 additional argument:" << "\n";
+    std::cerr << "  input filename (default \"" << fileName << "\", extension defaults to \"" << extensionDefault << "\")" << "\n";
+  }
+
+	auto t1 {std::chrono::high_resolution_clock::now()};
+
+	std::ifstream inputFile {fileName};
+	if (!inputFile)
+	{
+		std::cerr << "Input file \"" << fileName << "\" could not be opened\n";
+		return 1;
+	}
+
+	std::string inputStr {};
+
+	while (std::getline(inputFile,inputStr))
+	{
+
+	}
+
+	inputFile.close();
+
+	auto t2 {std::chrono::high_resolution_clock::now()};
+
+
+
+  auto t3 {std::chrono::high_resolution_clock::now()};
+
+
+
+  auto t4 {std::chrono::high_resolution_clock::now()};
+
+	std::cout << "Program took, in microseconds:" << "\n";
+	std::cout << "  Total Time:               " << std::chrono::duration_cast<std::chrono::microseconds>(t4-t1).count() << "\n";
+	std::cout << "  Reading in Input File:    " << std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count() << "\n";
+	std::cout << "  Problem Solving (Part 1): " << std::chrono::duration_cast<std::chrono::microseconds>(t3-t2).count() << "\n";
+	std::cout << "  Problem Solving (Part 2): " << std::chrono::duration_cast<std::chrono::microseconds>(t4-t3).count() << "\n";
+
+
+	return 0;
+}
