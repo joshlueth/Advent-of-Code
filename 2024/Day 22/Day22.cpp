e641a3cfb135662357668569a59520a3bbb58d5f
Joshua Lueth <lueth020@umn.edu>
2024-12-29 14:21:47 -0600

    Cut runtime down to .88s by avoiding using a vector, reducing redundant operations

--- 2024/Day 22/Day22.cpp
+++ 2024/Day 22/Day22.cpp
@@ -52,49 +52,44 @@ int main(int argc, char* argv[])
 
 	auto t2 {std::chrono::high_resolution_clock::now()};
 
-  int loops {2000};
-  std::map<std::vector<int>,long long> seq {};
+  int loops {2000}; // 2001 secret numbers if we count the first one
+  std::map<int,long long> seq {};
   for (std::size_t sn {0}; sn<iniSecret.size(); ++sn) {
-    std::vector<int> prices (4,0);
-    int current_price {price(iniSecret[sn])}, old_price {0}, price_change {0};
-    std::set<std::vector<int>> seq_lst {};
+    int current_price {price(iniSecret[sn])}, old_price {0}, old_price_change_m3 {0}, old_price_change_m2 {0}, old_price_change_m1 {0}, price_change {0};
+    std::set<int> seq_lst {};
     for (int lp {0}; lp<loops; ++lp) {
       evolve(iniSecret[sn]);
 
       old_price = current_price;
       current_price = price(iniSecret[sn]);
+
+      old_price_change_m3 = old_price_change_m2;
+      old_price_change_m2 = old_price_change_m1;
+      old_price_change_m1 = price_change;
       price_change = current_price - old_price;
-      std::rotate(prices.begin(), prices.begin()+1, prices.end());
-      prices[3] = price_change;
+
+      int hsh {sequence_hash(price_change, old_price_change_m1, old_price_change_m2, old_price_change_m3)};
+
       // lp: 0, first price change (pc); 1, second pc; 2, third pc; 3, fourth pc - finally have a sequence
-      if (lp>=3 && seq_lst.find(prices)==seq_lst.end()) {
-        seq[prices] += current_price;
-        seq_lst.insert(prices);
-        // std::cout << prices[0] << " " << prices[1] << " " << prices[2] << " " << prices[3] << " " << current_price << "\n";
+      if (lp>=3 && seq_lst.insert(hsh).second) {
+        seq[hsh] += current_price;
       }
-    }
-  }
-
-  long long sum {0};
-  for (std::size_t sn {0}; sn<iniSecret.size(); ++sn) {
-    // std::cout << sn << "\n";
-    sum += iniSecret[sn];
-  }
-
-  long long bananas {0};
-  for (auto entry : seq) {
-    if (entry.second > bananas) {
-      bananas = entry.second;
-      // for (auto ch : entry.first) {
-      //   std::cout << ch << " ";
-      // }
-      // std::cout << "\n";
     }
   }
 
   auto t3 {std::chrono::high_resolution_clock::now()};
 
+  long long sum {0};
+  for (std::size_t sn {0}; sn<iniSecret.size(); ++sn) {
+    sum += iniSecret[sn];
+  }
 
+  long long bananas {0};
+  for (const auto& entry : seq) {
+    if (entry.second > bananas) {
+      bananas = entry.second;
+    }
+  }
 
   auto t4 {std::chrono::high_resolution_clock::now()};
 
@@ -102,10 +97,10 @@ int main(int argc, char* argv[])
   std::cout << "Maximum Number of Bananas is: " << bananas << "\n";
 
 	std::cout << "Program took, in microseconds:" << "\n";
-	std::cout << "  Total Time:               " << std::chrono::duration_cast<std::chrono::microseconds>(t4-t1).count() << "\n";
-	std::cout << "  Reading in Input File:    " << std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count() << "\n";
-	std::cout << "  Problem Solving (Part 1): " << std::chrono::duration_cast<std::chrono::microseconds>(t3-t2).count() << "\n";
-	std::cout << "  Problem Solving (Part 2): " << std::chrono::duration_cast<std::chrono::microseconds>(t4-t3).count() << "\n";
+	std::cout << "  Total Time:            " << std::chrono::duration_cast<std::chrono::microseconds>(t4-t1).count() << "\n";
+	std::cout << "  Reading in Input File: " << std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count() << "\n";
+	std::cout << "  Problem Solving:       " << std::chrono::duration_cast<std::chrono::microseconds>(t3-t2).count() << "\n";
+	std::cout << "  Answer Determining:    " << std::chrono::duration_cast<std::chrono::microseconds>(t4-t3).count() << "\n";
 
 
 	return 0;
@@ -127,14 +122,23 @@ long long evolve (long long& secret_num) {
   return secret_num;
 }
 
-long long mix (long long num, long long new_num) {
+long long mix (const long long& num, const long long& new_num) {
   return num ^ new_num;
 }
 
-long long prune (long long num) {
+long long prune (const long long& num) {
   return num % 16777216;
 }
 
-int price(long long num) {
-  return std::to_string(num).back() - '0';
+int price(const long long& num) {
+  return num % 10;
+}
+
+int sequence_hash(const int& pm1, const int& pm2, const int& pm3, const int& pm4) {
+  int hsh {};
+  hsh += pm4 + 9;
+  hsh += 19 * (pm3 + 9);
+  hsh += 19 * 19 * (pm2 + 9);
+  hsh += 19 * 19 * 19 * (pm1 + 9);
+  return hsh;
 }
--- 2024/Day 22/Day22.h
+++ 2024/Day 22/Day22.h
@@ -1,6 +1,7 @@
 #pragma once
 
 long long evolve (long long& );
-long long mix (long long , long long );
-long long prune (long long );
-int price(long long );
+long long mix (const long long& , const long long& );
+long long prune (const long long& );
+int price(const long long& );
+int sequence_hash(const int& , const int& , const int& , const int& );
