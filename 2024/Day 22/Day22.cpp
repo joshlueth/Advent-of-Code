2deda945c97f2aae3966ad3f6c143c2f1510d8bc
Joshua Lueth <lueth020@umn.edu>
2024-12-29 05:40:08 -0600

    Solved Day 22, parts 1 and 2. Fairly easy, but takes ~25ms for part 1 and ~2s for part 2. Can be optimized

--- 2024/Day 22/Day22.cpp
+++ 2024/Day 22/Day22.cpp
@@ -3,6 +3,10 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <set>
 
 int main(int argc, char* argv[])
 {
@@ -34,23 +38,68 @@ int main(int argc, char* argv[])
 	}
 
 	std::string inputStr {};
-
+  std::vector<long long> iniSecret {};
 	while (std::getline(inputFile,inputStr))
 	{
-
+    iniSecret.push_back(std::stoll(inputStr));
 	}
 
 	inputFile.close();
 
+  // for (auto sn : iniSecret) {
+  //   std::cout << sn << "\n";
+  // }
+
 	auto t2 {std::chrono::high_resolution_clock::now()};
 
+  int loops {2000};
+  std::map<std::vector<int>,long long> seq {};
+  for (std::size_t sn {0}; sn<iniSecret.size(); ++sn) {
+    std::vector<int> prices (4,0);
+    int current_price {price(iniSecret[sn])}, old_price {0}, price_change {0};
+    std::set<std::vector<int>> seq_lst {};
+    for (int lp {0}; lp<loops; ++lp) {
+      evolve(iniSecret[sn]);
 
+      old_price = current_price;
+      current_price = price(iniSecret[sn]);
+      price_change = current_price - old_price;
+      std::rotate(prices.begin(), prices.begin()+1, prices.end());
+      prices[3] = price_change;
+      // lp: 0, first price change (pc); 1, second pc; 2, third pc; 3, fourth pc - finally have a sequence
+      if (lp>=3 && seq_lst.find(prices)==seq_lst.end()) {
+        seq[prices] += current_price;
+        seq_lst.insert(prices);
+        // std::cout << prices[0] << " " << prices[1] << " " << prices[2] << " " << prices[3] << " " << current_price << "\n";
+      }
+    }
+  }
+
+  long long sum {0};
+  for (std::size_t sn {0}; sn<iniSecret.size(); ++sn) {
+    // std::cout << sn << "\n";
+    sum += iniSecret[sn];
+  }
+
+  long long bananas {0};
+  for (auto entry : seq) {
+    if (entry.second > bananas) {
+      bananas = entry.second;
+      // for (auto ch : entry.first) {
+      //   std::cout << ch << " ";
+      // }
+      // std::cout << "\n";
+    }
+  }
 
   auto t3 {std::chrono::high_resolution_clock::now()};
 
 
 
   auto t4 {std::chrono::high_resolution_clock::now()};
+
+  std::cout << "Sum of 2000th Secret Numbers is: " << sum << "\n";
+  std::cout << "Maximum Number of Bananas is: " << bananas << "\n";
 
 	std::cout << "Program took, in microseconds:" << "\n";
 	std::cout << "  Total Time:               " << std::chrono::duration_cast<std::chrono::microseconds>(t4-t1).count() << "\n";
@@ -60,4 +109,32 @@ int main(int argc, char* argv[])
 
 
 	return 0;
+}
+
+long long evolve (long long& secret_num) {
+  long long new_num {secret_num * 64LL};
+  secret_num = mix(secret_num, new_num);
+  secret_num = prune(secret_num);
+  
+  new_num = secret_num / 32LL;
+  secret_num = mix(secret_num, new_num);
+  secret_num = prune(secret_num);
+
+  new_num = secret_num * 2048LL;
+  secret_num = mix(secret_num, new_num);
+  secret_num = prune(secret_num);
+
+  return secret_num;
+}
+
+long long mix (long long num, long long new_num) {
+  return num ^ new_num;
+}
+
+long long prune (long long num) {
+  return num % 16777216;
+}
+
+int price(long long num) {
+  return std::to_string(num).back() - '0';
 }
--- 2024/Day 22/Day22.h
+++ 2024/Day 22/Day22.h
@@ -1,2 +1,6 @@
 #pragma once
 
+long long evolve (long long& );
+long long mix (long long , long long );
+long long prune (long long );
+int price(long long );
