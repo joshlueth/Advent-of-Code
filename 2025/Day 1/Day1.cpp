c81db581bad714be11de6f25a58dbf0679cd74fe
Joshua Lueth <lueth020@umn.edu>
2025-12-01 21:57:04 -0600

    Solved Day 1, 2025

--- 2025/Day 1/Day1.cpp
+++ 2025/Day 1/Day1.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <vector>
+#include <cmath>
 
 int main(int argc, char* argv[])
 {
@@ -34,23 +36,74 @@ int main(int argc, char* argv[])
 	}
 
 	std::string inputStr {};
+  std::vector<std::string> inputs {};
 
 	while (std::getline(inputFile,inputStr))
 	{
-
+    inputs.push_back(inputStr);
 	}
 
 	inputFile.close();
 
 	auto t2 {std::chrono::high_resolution_clock::now()};
 
-
+  int counter1 {0};
+  int dial {50};
+  for (std::size_t i=0; i<inputs.size(); i++) {
+    char dir {inputs[i][0]};
+    int rot {std::stoi(inputs[i].substr(1))};
+    if (dir=='L') {
+      rot = -rot;
+    } else if (dir=='R') {} else {
+      std::cerr << "Must rotate either left (L) or right (R)\n";
+      return 0;
+    }
+    dial = (dial + rot);
+    if (dial % 100==0) counter1 += 1;
+  }
 
   auto t3 {std::chrono::high_resolution_clock::now()};
 
+  int counter2 {0};
+  int dial_new {0};
+  dial = 50;
+  for (std::size_t i=0; i<inputs.size(); i++) {
+    char dir {inputs[i][0]};
+    int rot {std::stoi(inputs[i].substr(1))};
+    if (dir=='L') {
+      rot = -rot;
+    } else if (dir=='R') {} else {
+      std::cerr << "Must rotate either left (L) or right (R)\n";
+      return 0;
+    }
+    // we have initial value and subsequent value.
+    // we want to determine the amount of hundreds crossed over
+    // different buckets: -199:-101; -100; -99:-1; 0; 1:99; 100; 101:199; 200; 201:299; 300
+    // if we rotate to the right, each number divisible by zero is associated with the bucket to the right
+    // if we rotate to the left, each number divisible by zero is associated with the bucket to the left
+    dial_new = dial + rot;
 
+    if (rot>0) {
+      int h_i = static_cast<int>(std::floor(dial/100.));
+      int h_f = static_cast<int>(std::floor(dial_new/100.));
+      counter2 += std::abs(h_f-h_i);
+      // std::cout << dial << " " << dial_new << " " << h_i << " " << h_f << " " << abs(h_f-h_i) << " " << counter2 << "\n";
+    } else {
+      int h_i = static_cast<int>(std::ceil(dial/100.));
+      int h_f = static_cast<int>(std::ceil(dial_new/100.));
+      counter2 += std::abs(h_f-h_i);
+      // std::cout << dial << " " << dial_new << " " << h_i << " " << h_f << " " << abs(h_f-h_i) << " " << counter2 << "\n";
+    }
+
+    dial = dial_new; 
+
+  }
 
   auto t4 {std::chrono::high_resolution_clock::now()};
+
+  std::cout << "\n";
+  std::cout << "Part 1: " << counter1 << "\n";
+  std::cout << "Part 2: " << counter2 << "\n\n";
 
 	std::cout << "Program took, in microseconds:" << "\n";
 	std::cout << "  Total Time:               " << std::chrono::duration_cast<std::chrono::microseconds>(t4-t1).count() << "\n";
